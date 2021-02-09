f3caf3354bc24dc2b2e29b167701fd7fd69de78e
TeddyGlass <kota.kurosaki0226@gmail.com>
2021-02-09 20:50:58 +0900

    settings

--- django_website2/settings.py
+++ django_website2/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from .settings_secret import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't)o#+t*)h$mppuk07pr5hss@pkmzlmxqw#er83j(lf$n(f#xbe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
